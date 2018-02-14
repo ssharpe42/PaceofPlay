dcc87b3de559d66b2606c9e539c1c45a657c8fcc
ssharpe42 <ssharpe42y@gmail.com>
2018-02-13 21:36:11 -0500

    Add comments/graphics

--- R/Graphics.R
+++ R/Graphics.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(ggthemes)
+library(ggridges)
 library(scales)
+library(gridExtra)
 
 #Load data
 batter_pace=readRDS('data/batter_pace.RDS')
@@ -8,7 +10,7 @@ pitcher_pace = readRDS('data/pitcher_pace.RDS')
 pace_data = readRDS('data/pace_granular.RDS')
 pitches_total = read.csv('data/fg_pitchinfo.csv') %>%
     filter(Season>=2008)
-
+Results = readRDS('data/result_tibble.RDS')
 
 #Time function
 to_time = function(x) paste0(floor(x),':',str_pad(round(60*(x-floor(x))),width = 2, pad = '0'))
@@ -36,11 +38,7 @@ pitches_total %>%
     mutate(Value = Value- first(Value)) %>% spread(Metric, Value)%>%View()
 
 
-#Batter and pitcher pace
-ggplot(batter_pace %>% filter(PA>400))+
-    geom_density(aes(exp_min_added_game , fill = factor(season)))
-
-
+#Game time distribution
 game_time = readRDS('data/game_time.RDS')
 
 plot_time = game_time %>% filter(!is.na(time))%>%
@@ -61,7 +59,8 @@ ggplot(plot_time)+
           text = element_text(size = 16),
           axis.title = element_text(size = 16),
           title = element_text(size = 14))+
-    ggtitle("Distribution of Game Times (2010-2017)")
+    ggtitle("Distribution of Game Times (2010-2017)")+
+    labs(caption = 'Source: PITCHf/x Data')
 
 
 #### State Pace Graphs ####
@@ -92,7 +91,8 @@ ggplot(state_pace)+
           legend.key.height = unit(1,'cm'),
           panel.grid.minor = element_line(color='gray'),
           panel.grid.major = element_line(color='gray'))+
-    ylim(0, 30)
+    ylim(0, 30)+
+    labs(caption = "Source: PITCHF/x Data")
 
 #Pie legend
 filter(pace_data, season==2017)%>%summarise(pace=mean(pace))%>% ggplot()+ geom_bar(aes(x =1, y = pace), fill = 'black', alpha = .3, stat ='identity')+coord_polar(theta = "y")+
@@ -119,7 +119,7 @@ inning_pace= filter(pace_data, season==2017) %>%
 
 #Line graph
 ggplot(inning_pace)+
-    geom_line(aes(x = inning, y = pace, colour = inning_side, group = inning_side), size = 1)+
+    geom_line(aes(x = inning, y = pace, colour = inning_side, group = inning_side), size = 1.5)+
     theme_fivethirtyeight()+
     scale_x_discrete("\nInning")+
     scale_y_continuous("Pace\n", label = to_sec)+
@@ -127,27 +127,31 @@ ggplot(inning_pace)+
     theme(text = element_text(size = 16),
           axis.title = element_text(size = 16),
           title = element_text(size = 14)) +
-    ggtitle("Average Pace by Inning")
+    ggtitle("Average Pace by Inning (2017)")+
+    labs(caption = "Source: PITCHF/x Data")
 
 
 #### Pace by pitch type #####
 pitch_pace = filter(pace_data, season==2017) %>%
     group_by(prev_des)%>%
     summarise(pace = mean(pace))%>%
-    mutate(prev_des = ifelse(prev_des=='Foul (Runner Going)','Foul\n(Runner Going)', prev_des))
+    mutate(prev_des = ifelse(prev_des=='Foul (Runner Going)','Foul\n(Runner Going)', prev_des))%>%
+    arrange(pace)%>%
+    mutate(prev_des = factor(prev_des, levels = prev_des, ordered = T))
 
 #Bar graph
 ggplot(pitch_pace)+
     geom_bar(aes(x = prev_des, y = pace), fill = "#014d64", stat = 'identity')+
     theme_fivethirtyeight()+
     scale_fill_economist()+
-    scale_y_continuous("Pace\n")+
+    scale_y_continuous("Pace\n", labels = to_sec)+
     scale_x_discrete("\n Previous Pitch")+
     theme(text = element_text(size = 16),
           axis.title = element_text(size = 16),
           title = element_text(size = 14)) +
     guides(fill=FALSE)+
-    ggtitle("Pace Given Previous Pitch Result")
+    ggtitle("Pace Given Previous Pitch Result (2017)")+
+    labs(caption = "Source: PITCHF/x Data")
 
 
 #### Pace by count #####
@@ -159,32 +163,242 @@ count_pace = filter(pace_data, season==2017) %>%
 #Bar graph
 ggplot(count_pace)+
     geom_tile(aes(x = Strikes, y = Balls, fill = pace))+
-    geom_text(aes(x = Strikes, y = Balls, label = to_sec(round(pace,1))))+
+    #geom_text(aes(x = Strikes, y = Balls, label = to_sec(round(pace,1))))+
     theme_fivethirtyeight()+
-    scale_fill_distiller('Pace  ', palette = 'RdBu')+
+    scale_fill_distiller('Pace     ', palette = 'RdBu', labels = to_sec, breaks = c(20,23,26))+
     scale_y_discrete("Balls\n")+
     scale_x_discrete("\n Strikes")+
     theme(text = element_text(size = 16),
           axis.title = element_text(size = 16),
           title = element_text(size = 14),
           legend.key.width = unit(1, "cm")) +
-    ggtitle("Pace by Count")
+    ggtitle("Pace by Count (2017)")+
+    labs(caption = "Source: PITCHF/x Data")
 
 
 #### Pace by Score ####
 score_pace = filter(pace_data, season == 2017) %>%
     group_by(score_diff )%>%
-    summarise(pace = mean(pace))
+    summarise(pace = mean(pace), n = n())
 
 filter(pace_data, season == 2017) %>%
     ggplot()+
-    stat_smooth(aes(x = score_diff, y = pace), method = 'lm', formula = 'y~poly(x, 3)')+
-    geom_point(data = score_pace , aes(x = score_diff, y = pace, color = 'Average Pace\n(at each score)'))+
+    stat_smooth(aes(x = score_diff, y = pace ), method = 'lm', formula = 'y~poly(x, 3)')+
+    geom_point(data = score_pace , aes(x = score_diff, y = pace, size =n))+
     theme_fivethirtyeight()+
     scale_color_manual("", values = c('black','black'))+
-    scale_y_continuous("Pace\n")+
+    scale_size_continuous("     Pitches\n(at each score diff.)", labels = function(x) paste0(floor(x/1000),'k'))+
+    scale_y_continuous("Pace\n", labels = to_sec)+
     scale_x_continuous("\nHome Score Difference")+
     theme(text = element_text(size = 16),
           axis.title = element_text(size = 16),
           title = element_text(size = 14)) +
-    ggtitle("Pace by Count (2017)")
+    ggtitle("Pace by Score Differential (2017)")+
+    labs(caption = "Source: PITCHF/x Data")
+
+
+#### Year to Year Correlation cPace vs Pace ####
+
+# Batters
+qual_batters = filter(batter_pace, PA>100)
+
+year_to_year_b = bind_rows(left_join(filter(qual_batters, season==2015)  %>%
+              select(batter_name, cpace1=cpace, pace1=pace), 
+          filter(qual_batters, season==2016)  %>%
+              select(batter_name, cpace2=cpace, pace2=pace)),
+          left_join(filter(qual_batters, season==2016)  %>%
+                        select(batter_name, cpace1=cpace, pace1=pace), 
+                    filter(qual_batters, season==2017)  %>%
+                        select(batter_name, cpace2=cpace, pace2=pace)))%>%
+    filter(!is.na(cpace2))
+
+#plot pace         
+pace_corr_b = ggplot(year_to_year_b,aes(x = pace1, y = pace2))+
+    geom_point()+
+    geom_smooth(method = 'lm')+
+    geom_text(x =22, y = 30, label = paste('Rsq = ',round(summary(lm(pace2~pace1, year_to_year_b))$r.squared,3)), size = 5)+
+    scale_x_continuous("\nPace in Year 1",limits = c(18, 32), breaks =seq(18,32,2))+
+    scale_y_continuous("Pace in Year 2\n",limits = c(18, 32),breaks =seq(18,32,2))+
+    ggtitle('Batter Pace Year 1 vs. Year 2', subtitle = 'At Least 100 PA (2015-2017)')+
+    theme_fivethirtyeight()+
+    theme(text = element_text(size = 14),
+          axis.title = element_text(size = 14),
+          title = element_text(size = 12))
+
+#plot cpace
+cpace_corr_b = ggplot(year_to_year_b,aes(x = cpace1, y = cpace2))+
+    geom_point()+
+    geom_smooth(method = 'lm')+
+    geom_text(x =-1, y =6, label = paste('Rsq = ',round(summary(lm(cpace2~cpace1, year_to_year_b))$r.squared,3)), size = 5)+
+    scale_x_continuous("\ncPace in Year 1",limits = c(-4, 7), breaks =seq(-4,6,2))+
+    scale_y_continuous("cPace in Year 2\n",limits = c(-4, 7),breaks =seq(-4,6,2))+
+    ggtitle('Batter cPace Year 1 vs. Year 2', subtitle = 'At Least 100 PA (2015-2017)')+
+    theme_fivethirtyeight()+
+    theme(text = element_text(size = 14),
+          axis.title = element_text(size = 14),
+          title = element_text(size = 12))
+          
+
+# Pitchers
+qual_pitchers = filter(pitcher_pace, PA>100)
+
+year_to_year_p = bind_rows(left_join(filter(qual_pitchers, season==2015)  %>%
+                                       select(pitcher_name, cpace1=cpace, pace1=pace), 
+                                   filter(qual_pitchers, season==2016)  %>%
+                                       select(pitcher_name, cpace2=cpace, pace2=pace)),
+                         left_join(filter(qual_pitchers, season==2016)  %>%
+                                       select(pitcher_name, cpace1=cpace, pace1=pace), 
+                                   filter(qual_pitchers, season==2017)  %>%
+                                       select(pitcher_name, cpace2=cpace, pace2=pace)))%>%
+    filter(!is.na(cpace2))
+
+#plot pace
+pace_corr_p = ggplot(year_to_year_p,aes(x = pace1, y = pace2))+
+    geom_point()+
+    geom_smooth(method = 'lm')+
+    geom_text(x =22, y = 30, label = paste('Rsq = ',round(summary(lm(pace2~pace1, year_to_year_p))$r.squared,3)), size = 5)+
+    scale_x_continuous("\nPace in Year 1",limits = c(18, 32), breaks =seq(18,32,2))+
+    scale_y_continuous("Pace in Year 2\n",limits = c(18, 32),breaks =seq(18,32,2))+
+    ggtitle('Pitcher Pace Year 1 vs. Year 2', subtitle = 'At Least 100 Batters Faced (2015-2017)')+
+    theme_fivethirtyeight()+
+    theme(text = element_text(size = 14),
+          axis.title = element_text(size = 14),
+          title = element_text(size = 12))
+
+#plot cpace
+cpace_corr_p = ggplot(year_to_year_p,aes(x = cpace1, y = cpace2))+
+    geom_point()+
+    geom_smooth(method = 'lm')+
+    geom_text(x =-1, y =6, label = paste('Rsq = ',round(summary(lm(cpace2~cpace1, year_to_year_p))$r.squared,3)), size = 5)+
+    scale_x_continuous("\ncPace in Year 1",limits = c(-5, 7), breaks =seq(-4,7,2))+
+    scale_y_continuous("cPace in Year 2\n",limits = c(-5, 7),breaks =seq(-4,7,2))+
+    ggtitle('Pitcher cPace Year 1 vs. Year 2', subtitle = 'At Least 100 Batters Faced (2015-2017)')+
+    theme_fivethirtyeight()+
+    theme(text = element_text(size = 14),
+          axis.title = element_text(size = 14),
+          title = element_text(size = 12))
+
+#combine
+grid.arrange(pace_corr_b, cpace_corr_b,pace_corr_p,cpace_corr_p, nrow = 2) 
+
+
+year_to_year = bind_rows(year_to_year_b%>%mutate(type='Batter'),
+                         year_to_year_p%>%mutate(type='Pitcher')) %>%
+    gather(Metric1, Year1, cpace1, pace1)%>%
+    gather(Metric2, Year2, cpace2, pace2) %>%
+    mutate(Metric1 = gsub('1','',Metric1),
+           Metric2 = gsub('2','',Metric2)) %>%
+    filter(Metric1==Metric2)
+
+ggplot(year_to_year,aes(x = Year1, y = Year2))+
+    geom_point()+
+    geom_smooth(method = 'lm')+
+    facet_grid(type~Metric1,scales = 'free_y' )
+
+### Top 10/ Bottom 10 Exp Hours / Season 
+hrs_season = bind_rows(rename(batter_pace, name=batter_name)%>% mutate(type='Batters'),
+          rename(pitcher_pace, name=pitcher_name)%>% mutate(type='Pitchers'))%>%
+filter(season ==2017, PA>400)%>% 
+    group_by(type)%>%
+    arrange(cpace)%>%
+    filter(row_number()<=10 | (row_number()<=n() & row_number()>=(n()-9)))%>% 
+    ungroup%>%
+    mutate(name = factor(name, levels = name, ordered = T),
+           positive = cpace<0)
+
+ggplot(hrs_season)+
+    geom_bar(aes(x = name, y = exp_min_added_pa*10, fill = positive), stat = 'identity') +
+    coord_flip()+
+    scale_y_continuous("\nxHSAA\n\n(Based on 600 PA Season and Lg Avg Pitch/PA)", labels = function(x) paste(x,'hrs'))+
+    scale_x_discrete("Player\n")+
+    facet_wrap(~type, scales = 'free')+
+    theme_fivethirtyeight()+
+    guides(fill = F)+
+    theme(text = element_text(size = 16),
+          axis.title = element_text(size = 16),
+          title = element_text(size = 12))+
+    ggtitle("Expected Hours Added Per Season Above Average (xHSAA)",
+            subtitle = "Top/Bottom 10 players with at least 400 PA or BF")
+
+
+#### Rule Effects ####
+
+# No Runners 18 sec clock
+#https://stackoverflow.com/questions/46516759/fixed-fill-for-different-sections-of-a-density-plot-with-ggplot
+
+#Create density values for no runners on base:
+#  Keep plot values less than 1 min and >0
+#  Adjust pace (-5 sec) for time to throw back to pitcher
+dens_norunner = density(filter(pace_data, season==2017, pace<60,pace>5, bases=='___',
+                               prev_des %in% c('Ball','Swinging Strike','Called Strike'))$pace-5, 
+                        n = 2^10, adj = 2)[c('x','y')]
+
+dens_norunner = data.frame(dens_norunner)%>% 
+    #create sections to color (18 min pitch clock)
+    mutate(section = cut(x, breaks = c(-Inf, 18, Inf)))%>%
+    group_by(section) %>% 
+    #calculate total percentage in section
+    mutate(prob=round(sum(y)*mean(diff(x))*100) ,
+            prob_char = paste0(prob,"%"),
+            max_prob = max(prob)) %>%
+    ungroup%>%
+    #Label violations with percentage
+    mutate(Violation = ifelse(!grepl('-Inf', section),paste0('Clock Violation (',max_prob,'%)'),
+                              paste0('No Violation (',max_prob,'%)')))
+          
+
+no_runner = ggplot(dens_norunner, aes(x, y, fill=Violation)) +
+    geom_area()+
+    scale_fill_discrete("")+
+    scale_x_continuous("\nPace", labels = to_sec)+
+    scale_y_continuous("")+
+    theme_fivethirtyeight()+
+    theme(text = element_text(size = 16),
+          axis.title = element_text(size = 16),
+          title = element_text(size = 12),
+          axis.text.y = element_blank(),
+          legend.position = c(.8,.5),
+          legend.direction = 'vertical')+
+    ggtitle("No Runners on Base: 18 Second Pitch Clock")
+
+# Runners 20 sec clock
+#https://stackoverflow.com/questions/46516759/fixed-fill-for-different-sections-of-a-density-plot-with-ggplot
+#Calculate density values 
+
+#Create density values for runners on base:
+#  Keep plot values less than 1 min and >0
+#  Adjust pace (-5 sec) for time to throw back to pitcher
+dens_runner = density(filter(pace_data, season==2017, pace<60,pace>5, bases!='___',
+                             prev_des %in% c('Ball','Swinging Strike','Called Strike'))$pace-5, 
+                      n = 2^10, adj = 2)[c('x','y')]
+
+dens_runner = data.frame(dens_runner)%>% 
+    #create sections to color (20 min pitch clock)
+    mutate(section = cut(x, breaks = c(-Inf, 20, Inf)))%>%
+    group_by(section) %>% 
+    #calculate total percentage in section
+    mutate(prob=round(sum(y)*mean(diff(x))*100) ,
+           prob_char = paste0(prob,"%"),
+           max_prob = max(prob)) %>%
+    ungroup%>%
+    #Label violations with percentage
+    mutate(Violation = ifelse(!grepl('-Inf', section),paste0('Clock Violation (',max_prob,'%)'),
+                              paste0('No Violation (',max_prob,'%)')))
+
+runner = ggplot(dens_runner, aes(x, y, fill=Violation)) +
+    geom_area()+
+    scale_fill_discrete("")+
+    scale_x_continuous("\nPace" , labels = to_sec)+
+    scale_y_continuous("")+
+    theme_fivethirtyeight()+
+    theme(text = element_text(size = 16),
+          axis.title = element_text(size = 16),
+          title = element_text(size = 12),
+          axis.text.y = element_blank(),
+          legend.position = c(.8,.5),
+          legend.direction = 'vertical')+
+    ggtitle("Runners on Base: 20 Second Pitch Clock")+
+    labs(caption = "Source: PITCHF/x Data")
+
+#Combine plots into one
+grid.arrange(no_runner, runner)
--- R/PitchTime.R
+++ R/PitchTime.R
@@ -18,22 +18,19 @@ dbRetro=dbConnect(MySQL(),user="root",
                   password="root",
                   unix.sock="/Applications/MAMP/tmp/mysql/mysql.sock")
 
-#Retrosheet event files
-#retro = fetch(dbSendQuery(dbRetro, paste0("select * from events_bck where YEAR_ID=",Season)), n = -1)
-#retro[, grepl('_FL$', names(retro))] = lapply(retro[, grepl('_FL$', names(retro))], as.logical)
-
-#Game Logs
+#Retrosheet Game Logs
 game_logs = fetch(dbSendQuery(dbRetro, paste0("select * from games_bck where YEAR_ID=2015 OR YEAR_ID=2016 OR YEAR_ID=2017")),n = -1) %>%
     mutate(gameday_link = paste0('gid_',substr(GAME_ID, 4,7),'_',substr(GAME_ID, 8,9),'_',substr(GAME_ID, 10,11),'_',
                                  tolower(AWAY_TEAM_ID),'mlb', '_',tolower(substr(GAME_ID, 1,3)),'mlb','_', pmax(as.integer(substr(GAME_ID,12,12)),1)))
 
+#IDS: MLB to Name
 ids =select(read.csv('https://raw.githubusercontent.com/chadwickbureau/register/master/data/people.csv'),
             key_mlbam, key_retro, name_last ,name_first ) %>%
     mutate(Name = paste(name_first, name_last)) %>%
     select(-name_last, -name_first)%>%
     filter(!(is.na(key_mlbam)|key_mlbam==''|is.na(key_retro)|key_retro==''))
 
-# Gather More Pitch Specific Data from Pitchf/x
+# Gather Data from Pitchf/x
 PITCH <- collect(filter(tbl(db, 'pitch'),gameday_link %in% game_logs$gameday_link),n=Inf) %>%
     mutate(event_num = as.integer(event_num), num = as.integer(num), season = as.integer(substr(gameday_link, 5, 8)))
 
@@ -50,52 +47,68 @@ PO <- collect(filter(tbl(db, 'po'),gameday_link %in% game_logs$gameday_link),n=I
 #     mutate(event_num = as.integer(event_num),num = as.integer(num))
 
 
-AllEvents = bind_rows(select(ACTION, url, gameday_link, num, event_num, des, a_event=event, tfs_zulu) %>% 
+AllEvents = bind_rows(
+    #All actions (Injuries, Defensive Subs, Reviews, Ejections, etc)
+    select(ACTION, url, gameday_link, num, event_num, des, a_event=event, tfs_zulu) %>% 
                           mutate(type = 'aciton',
                                  time = as_datetime(gsub('T|Z',' ',tfs_zulu))),
-                      select(PO, url, gameday_link, num, event_num, des) %>% mutate(type= 'po'),
-                      PITCH %>% mutate(type = 'pitch') %>%
-                          mutate(time = as_datetime(gsub('T|Z',' ',tfs_zulu))) %>%
-                          group_by(url, num) %>%
-                          arrange(event_num) %>%
-                          mutate(pace = as.numeric(time-lag(time))) %>%
-                          ungroup) %>%
+    #All Pick off attampts
+    select(PO, url, gameday_link, num, event_num, des) %>% mutate(type= 'po'),
+    #All pitches
+    PITCH %>% mutate(type = 'pitch') %>%
+        mutate(time = as_datetime(gsub('T|Z',' ',tfs_zulu))) %>%
+        #For each PA calculate pace between pitches
+        group_by(url, num) %>%
+        arrange(event_num) %>%
+        mutate(pace = as.numeric(time-lag(time))) %>%
+        ungroup
+    ) %>%
+    #Arrange by game, play number, type of play
     arrange(url, gameday_link, num, event_num, desc(type)) %>%
     group_by(url, gameday_link,num) %>%
+    #Get the previous event and description
     mutate(prev_event = lag(a_event), prev_des = lag(des)) %>%
     ungroup
 
 
 
 EligiblePitch = filter(AllEvents, 
+                       #dont include events without pace
                        !is.na(pace), 
+                       #take out plays without prev_event (i.e., event was not a pitch)
                        is.na(prev_event),
+                       #take out remaining pickoffs
                        !grepl('Pickoff', prev_des),
+                       #only pace above 0 (some data errors)
                        pace>0,
-                       pace<180
-)%>%
+                       #some really long pace (rain delays etc)
+                       #180 seconds is the 99.98 percentile
+                       pace<180)%>%
+    #Join AB data
     left_join(ATBAT %>% 
                   rename(event_num_b = event_num)%>%
                   group_by(url, inning, inning_side) %>% 
                   arrange(num)%>%
                   mutate(outs = lag(o, default = 0)), by = c('url','num','inning_side','inning','gameday_link')) %>%
+    #A few incorrect plays labeled w/ 3 outs
     filter(outs!=3)%>%
+    #Calculate a few necessary columns
     mutate(inning =ifelse(as.integer(inning)<9, inning, "9+"),
            home_team_runs = as.integer(home_team_runs),
            away_team_runs = as.integer(away_team_runs),
            bases = gsub("0", "_", paste0(as.integer(!is.na(on_1b)),as.integer(!is.na(on_2b))*2,as.integer(!is.na(on_3b))*3)),
            score_diff = ifelse(inning_side=='top', home_team_runs-away_team_runs, away_team_runs-home_team_runs),
            h_score_diff = home_team_runs-away_team_runs, 
+           #standardize the previous pitch into similar categories
            prev_des = recode(prev_des, 'Automatic Ball'='Ball',
                              'Missed Bunt' = 'Swinging Strike',
                              'Pitchout'='Ball',
                              'Foul Tip' = 'Swinging Strike',
                              'Swinging Strike (Blocked)'='In Dirt',
                              'Ball In Dirt'='In Dirt'),
-           count_txt = ifelse(count %in% c('0-1','0-2','1-2'), 'Pitcher Ahead',
-                              ifelse(count %in% c('1-0','2-0','2-1','3-0','3-1'), 'Batter Ahead','Even')),
            season = as.integer(substr(gameday_link,5,8))) %>%
     select(-batter_name, -pitcher_name) %>%
+    #Get player names
     left_join(ids %>% select(batter = key_mlbam, batter_name = Name, -key_retro)) %>%
     left_join(ids %>% select(pitcher= key_mlbam, pitcher_name = Name, -key_retro))
 
@@ -109,10 +122,12 @@ pace_model = function(df){
              (1|batter) + (1 |pitcher) , data=df)
 }
 
+#Map pace_model to data for each season
 Results = group_by(EligiblePitch, season)%>%
     nest()%>%
     mutate(model = map(data,pace_model))
 
+#Function to apply to lme4 model to extract rand effects
 get_rand_effects = function(mod){
     rand =  ranef(mod)
     
@@ -122,59 +137,62 @@ get_rand_effects = function(mod){
                                      cpace = rand$pitcher[,1])))
 }
 
+#Extract random effects
 Results = mutate(Results, rand_eff = map(model, get_rand_effects))
 
-# pace.lmer.2016<- lmer(pace ~ factor(outs)+bases + abs(score_diff) + abs(score_diff):(score_diff>=0)+ 
-#                      factor(inning)+inning_side + prev_des + count + 
-#                      (1|batter_name) + (1 |pitcher_name) , data=EligiblePitch %>% filter(season==2016))
-# 
-# pace.lmer.2017<- lmer(pace ~ factor(outs)+bases + abs(score_diff) + abs(score_diff):(score_diff>=0)+ 
-#                      factor(inning)+inning_side + prev_des + count + 
-#                      (1|batter_name) + (1 |pitcher_name) , data=EligiblePitch %>% filter(season==2017))
-# 
-
-# avg.pace = mean(EligiblePitch$pace)
-# 
-# EligiblePitch = mutate(EligiblePitch,pred_pace = predict(pace.lmer, EligiblePitch, re.form = NA))
-
+#Expected pitches per PA
 ex_pitch_pa = left_join(group_by(PITCH, season) %>% summarise(p = n()),
                         group_by(ATBAT, season)%>% summarise(ab = n())) %>%
     mutate(pppa = p/ab)
 
+#Ex PA per game
 ex_pa_game = group_by(ATBAT, season) %>% summarise(pa_game = n()/n_distinct(url)/18) 
 
 
 #Batter Results
 batter_pace = group_by(EligiblePitch, season, batter_name, batter) %>% 
+    #Pace and times between pitches
     summarise(pace = mean(pace),between_pitches = n())%>%
+    #join approx PA and games
     left_join(ATBAT %>% 
                   group_by(season, batter) %>% 
                   summarise(PA = n_distinct(url, num), 
                             games = n_distinct(url)))%>%
-    left_join( bind_rows(Results$rand_eff[[1]]$batter %>% mutate(season = 2016),
-                         Results$rand_eff[[2]]$batter %>% mutate(season = 2017))) %>%
+    #join random effects
+    left_join( do.call(rbind, lapply(1:3, function(i) Results$rand_eff[[i]]$batter%>%mutate(season = Results$season[i])))) %>%
+    #join expected measures
     left_join(ex_pitch_pa)%>%
     left_join(ex_pa_game)%>%
     ungroup%>%
+    #calculate min added per plate appearance
     mutate(min_added_pa = between_pitches*cpace/60/PA,
+           #expected min added per plate appearance (control for Pitches/PA)
            exp_min_added_pa = cpace*(pppa-1)/60,
+           #expected min added per game (control for # of PA)
            exp_min_added_game = pa_game*exp_min_added_pa) 
 
-pitcher_pace = group_by(EligiblePitch, season, pitcher_name, pitcher) %>% 
+pitcher_pace =  group_by(EligiblePitch, season, pitcher_name, pitcher) %>% 
+    #Pace and times between pitches
     summarise(pace = mean(pace),between_pitches = n())%>%
+    #join approx PA and games
     left_join(ATBAT %>% 
                   group_by(season, pitcher) %>% 
                   summarise(PA = n_distinct(url, num), 
                             games = n_distinct(url)))%>%
-    left_join( bind_rows(Results$rand_eff[[1]]$pitcher %>% mutate(season = 2016),
-                         Results$rand_eff[[2]]$pitcher %>% mutate(season = 2017))) %>%
+    #join random effects
+    left_join( do.call(rbind, lapply(1:3, function(i) Results$rand_eff[[i]]$pitcher%>%mutate(season = Results$season[i])))) %>%
+    #join expected measures
     left_join(ex_pitch_pa)%>%
     left_join(ex_pa_game)%>%
     ungroup%>%
+    #calculate min added per plate appearance
     mutate(min_added_pa = between_pitches*cpace/60/PA,
+           #expected min added per plate appearance (control for Pitches/PA)
            exp_min_added_pa = cpace*(pppa-1)/60,
+           #expected min added per game (control for # of PA)
            exp_min_added_game = pa_game*exp_min_added_pa) 
 
+#Save Data
 saveRDS(pitcher_pace,'data/pitcher_pace.RDS')
 saveRDS(batter_pace,'data/batter_pace.RDS')
 saveRDS(EligiblePitch,'data/pace_granular.RDS')
@@ -189,6 +207,9 @@ game_logs = fetch(dbSendQuery(dbRetro, paste0("select * from games_bck where YEA
 PITCH2 <- collect(filter(select(tbl(db, 'pitch'), tfs_zulu, gameday_link),gameday_link %in% game_logs$gameday_link),n=Inf) %>%
     mutate( season = as.integer(substr(gameday_link, 5, 8)))
 
-game_time = filter(PITCH2, !is.na(time)) %>%group_by(gameday_link, season) %>% summarise(time = max(time, na.rm=T)-min(time, na.rm=T)) 
+#Calculate game time: time between first pitch and last
+game_time = filter(PITCH2, !is.na(time)) %>%
+    group_by(gameday_link, season) %>% 
+    summarise(time = max(time, na.rm=T)-min(time, na.rm=T)) 
 
 saveRDS(game_time, 'data/game_time.RDS')
